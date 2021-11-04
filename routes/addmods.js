e8219f5e020403d42a9d27a46c25417cd4844932
hemanth97 <hsai7980@gmail.com>
2021-11-04 00:06:04 -0500

    Bonus

--- routes/addmods.js
+++ routes/addmods.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var query = req.query 
-console.log(`rows ?{10}`) 
-console.log(`cols &{3}`)
+console.log(`rows ${query.rows}`) 
+console.log(`cols ${query.cols}`)
 
     res.render('addmods', { title: `Addmods`,query: query });
 });
